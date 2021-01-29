12de5bba911ce9a97f6fc51dee40234e3fb8da97
Alex Gold <johnbark855@gmail.com>
2021-01-29 18:05:02 -0500

    added / to public

--- Routes/htmlRoutes.js
+++ Routes/htmlRoutes.js
@@ -7,6 +7,6 @@ module.exports = function (app) {
     });
 
     app.get("/notes", function (req, res) {
-        res.sendFile(path.join(__dirname, "..Public/notes.html"));
+        res.sendFile(path.join(__dirname, "../Public/notes.html"));
     });
 }
