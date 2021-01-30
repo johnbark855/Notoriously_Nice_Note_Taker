ebbb07bdd00c119dfba05960fe5a498338d57ada
Alex Gold <johnbark855@gmail.com>
2021-01-30 12:26:52 -0500

    newest updated files

---
+++ Routes/apiRoutes.js
@@ -0,0 +1,35 @@
+const fs = require ('fs');
+const noteData = require('../db/db.json')
+module.exports = function(app) {
+    let postNum = noteData.length;
+
+    app.get("/api/notes", function(req, res){
+    res.json(noteData);
+});
+
+app.post("/api/notes", function(req, res){
+    let saveNotes = req.body;
+    let id = "" + postNum;
+    saveNotes.id = id;
+    postNum = postNum + 1;
+    noteData.push(saveNotes);
+    fs.writeFile('./db/db.json',JSON.stringify(noteData), () => {
+        console.log("Write note successful.");});
+    
+    res.json(saveNotes);
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+    let chosen = req.params.id;
+
+    for (let i = 0; i < noteData.length; i++) {
+        if (chosen === noteData[i].id) {
+            data.splice(i,1);
+            fs.writeFile('./db/db.json',JSON.stringify(noteData), () => {
+                console.log("save successful.");});
+            };
+        };
+
+res.json(data);
+});
+}
