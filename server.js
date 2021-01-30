0d774a0bc767b17c5e4ef990604c4f80f677f9d4
Alex Gold <johnbark855@gmail.com>
2021-01-30 13:02:08 -0500

    updated server js file

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//dependencies
 const bodyParser = require ('body-parser');
 const express = require('express');
 const fs = require ('fs')
@@ -10,11 +11,11 @@ const PORT = process.env.PORT || 3002;
  
 app.use(express.urlencoded({ extended:true}));
 app.use(express.json());
-app.use('/assets',express.static('./assets'));
+app.use(express.static(path.join(__dirname, "public")));
 
 require ('./Routes/apiRoutes')(app);
 require ('./Routes/htmlRoutes')(app);
-
+//listen to port
 app.listen(PORT, function() {
     console.log('App is listening to PORT' + PORT);
 });
