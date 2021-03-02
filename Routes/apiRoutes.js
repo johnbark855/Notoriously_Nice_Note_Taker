2455526c6eac63e75cbbfad139b4b9e6fd34624e
john bark <johnbark855@gmail.com>
2021-03-02 14:50:57 -0500

    updated folders

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,35 +1,36 @@
 const fs = require ('fs');
-const noteData = require('../db/db.json')
-module.exports = function(app) {
-    let postNum = noteData.length;
+const notesData = require('../db/db.json')
+
+module.exports = function (app) {
+    let postNum = notesData.length;
 
     app.get("/api/notes", function(req, res){
-    res.json(noteData);
+    res.json(notesData);
 });
 
 app.post("/api/notes", function(req, res){
-    let saveNotes = req.body;
+    let saveNote = req.body;
     let id = "" + postNum;
-    saveNotes.id = id;
+    saveNote.id = id;
     postNum = postNum + 1;
-    noteData.push(saveNotes);
-    fs.writeFile('./db/db.json',JSON.stringify(noteData), () => {
+    notesData.push(saveNote);
+    fs.writeFile('./db/db.json',JSON.stringify(notesData), () => {
         console.log("Write note successful.");});
     
-    res.json(saveNotes);
+    res.json(saveNote);
 });
 
 app.delete("/api/notes/:id", function (req, res) {
     let chosen = req.params.id;
 
-    for (let i = 0; i < noteData.length; i++) {
-        if (chosen === noteData[i].id) {
-            data.splice(i,1);
-            fs.writeFile('./db/db.json',JSON.stringify(noteData), () => {
+    for (let i = 0; i < notesData.length; i++) {
+        if (chosen === notesData[i].id) {
+            notesData.splice(i,1);
+            fs.writeFile('./db/db.json',JSON.stringify(notesData), () => {
                 console.log("save successful.");});
             };
         };
 
-res.json(data);
+res.json(notesData);
 });
 }
